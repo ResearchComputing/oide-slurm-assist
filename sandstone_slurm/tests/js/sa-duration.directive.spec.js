0f59dd86a0dd86b6a9e8f6253f8792efec7c814f
Zebula Sampedro <sampedro@colorado.edu>
2016-10-03 16:50:05 -0600

    Adding validation tests.

--- sandstone_slurm/tests/js/sa-duration.directive.spec.js
+++ sandstone_slurm/tests/js/sa-duration.directive.spec.js
@@ -8,32 +8,82 @@ describe('sandstone.slurm.sa-duration', function() {
   describe('directive', function() {
     var $compile, $scope, isolateScope, element;
 
+    var setValue = function(v) {
+      $scope.model = '';
+      $scope.$digest();
+      $scope.model = v;
+      $scope.$digest();
+    };
+
     beforeEach(inject(function(_$compile_,_$rootScope_) {
+      var el = angular.element(baseElement);
       $compile = _$compile_;
       $scope = _$rootScope_.$new();
       $scope.minDuration = 1;
       $scope.maxDuration = 1500;
-      element = $compile(baseElement)($scope);
+      element = $compile(el)($scope);
       $scope.$digest();
       isolateScope = element.isolateScope();
     }));
 
-    it('updates min and max duration values when altered on $scope', function() {
-      // Alter max
-
-      // Alter min
-
-    });
-
     it('correctly validates the inputted duration', function() {
-      // No max
-
+      var el, tpl;
+      el = element[0];
       // No min
-
+      $scope.minDuration = undefined;
+      setValue('00:00:00');
+      tpl = el.outerHTML;
+      expect(tpl).not.toContain('ng-invalid-sa-duration');
+      // No max
+      $scope.minDuration = 1;
+      $scope.maxDuration = undefined;
+      setValue('00:01:00');
+      tpl = el.outerHTML;
+      expect(tpl).not.toContain('ng-invalid-sa-duration');
+      // Min + Max
+      $scope.minDuration = 1;
+      $scope.maxDuration = 4;
+      setValue('00:00:00');
+      tpl = el.outerHTML;
+      expect(tpl).toContain('ng-invalid-sa-duration');
+      setValue('00:05:00');
+      tpl = el.outerHTML;
+      expect(tpl).toContain('ng-invalid-sa-duration');
       // MM
-
+      $scope.minDuration = 1;
+      $scope.maxDuration = 4;
+      // valid
+      setValue('01');
+      tpl = el.outerHTML;
+      expect(tpl).not.toContain('ng-invalid-sa-duration');
+      // invalid
+      setValue('0');
+      tpl = el.outerHTML;
+      expect(tpl).toContain('ng-invalid-sa-duration');
+      setValue('05');
+      tpl = el.outerHTML;
+      expect(tpl).toContain('ng-invalid-sa-duration');
       // MM:SS
-
+      $scope.minDuration = 1;
+      $scope.maxDuration = 4;
+      // valid
+      setValue('01:30');
+      tpl = el.outerHTML;
+      expect(tpl).not.toContain('ng-invalid-sa-duration');
+      // invalid min
+      setValue('0:0');
+      tpl = el.outerHTML;
+      expect(tpl).toContain('ng-invalid-sa-duration');
+      setValue('00:59');
+      tpl = el.outerHTML;
+      expect(tpl).toContain('ng-invalid-sa-duration');
+      // invalid max
+      setValue('03:121');
+      tpl = el.outerHTML;
+      expect(tpl).toContain('ng-invalid-sa-duration');
+      setValue('05:00');
+      tpl = el.outerHTML;
+      expect(tpl).toContain('ng-invalid-sa-duration');
       // HH:MM:SS
 
       // DD-HH:MM:SS
