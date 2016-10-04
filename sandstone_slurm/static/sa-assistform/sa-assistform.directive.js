9b15f4da9e513409543b14cdad1461dc029bf800
Zebula Sampedro <sampedro@colorado.edu>
2016-10-04 16:53:13 -0600

    Revising typeaheadfocus directive.

--- sandstone_slurm/static/sa-assistform/sa-assistform.directive.js
+++ sandstone_slurm/static/sa-assistform/sa-assistform.directive.js
@@ -68,12 +68,6 @@ angular.module('sandstone.slurm')
         return fields;
       };
 
-      $scope.addField = function(prop) {
-        if (prop) {
-          $scope.fieldNames.push(prop);
-        }
-      };
-
       /**
         * @function removeField
         * @param {object} field The JSON schema property object corresponding to the
@@ -115,6 +109,19 @@ angular.module('sandstone.slurm')
       };
 
       /**
+        * @function addField
+        * This is the function bound to the typeahead add button.
+        */
+      $scope.addField = function() {
+        var sel = $scope.selectedProp;
+        var props = $scope.getProperties();
+        if (props.indexOf(sel) >= 0) {
+          $scope.fieldNames.push(sel);
+          $scope.selectedProp = '';
+        }
+      };
+
+      /**
         * @function onTypeaheadKey
         * This is the event handler bound to the typeahead field, called
         * when the user hits enter.
@@ -128,6 +135,28 @@ angular.module('sandstone.slurm')
             $scope.selectedProp = '';
           }
         }
+      };
+
+      /**
+        * @function propValid
+        * @returns {boolean} isValid Whether or not $scope.selectedProp is valid
+        */
+      $scope.propValid = function() {
+        var prop = $scope.selectedProp;
+
+        // field name unspecified
+        if (!prop) {
+          return false;
+        }
+        // field exists
+        if ($scope.fieldNames.indexOf(prop) >= 0) {
+          return false;
+        }
+        // field name invalid
+        if (!(prop in $scope.config.profiles[$scope.selectedProfile].schema.properties)) {
+          return false;
+        }
+        return true;
       };
 
       /**
